cd8cdb57c074fdf1e05dc3e8f2ded29547a2968e
Katharina Peter <gaz@gmx.net>
2016-04-09 18:06:08 -0700

    use glyph icons for edit/delete links

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -2,4 +2,9 @@ module RecipesHelper
   def title
     @recipe.try :name
   end
+  
+  def allow_edit?(recipe)
+    return false unless user_signed_in?
+    recipe.user_id == current_user.id
+  end
 end
