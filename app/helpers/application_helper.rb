43e4cacd10e4847f3fc8111c2c42bc6308beed2a
Katharina Peter <gaz@gmx.net>
2016-04-11 22:57:27 -0700

    added nav_link_to helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def current_user_id
     current_user.try :id
   end
+
+  def nav_link_to(name=nil, path="#", *args, &block)
+    path = name || path if block_given?
+    options = { class: 'dropdown-item nav-link' }
+    if block_given?
+      link_to path, options, &block
+    else
+      link_to name, path, options
+    end
+  end
 end
