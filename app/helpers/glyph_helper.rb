86867721442904c73b99ee87f1ac7338ba7e8a42
Katharina Peter <gaz@gmx.net>
2016-04-11 23:10:14 -0700

    use fixed-width icons in nav
    also simplified glyph helper method and added aria-hidden setting

--- app/helpers/glyph_helper.rb
+++ app/helpers/glyph_helper.rb
@@ -1,19 +1,7 @@
 module GlyphHelper
-  # ==== Examples
-  # glyph(:share_alt)
-  # # => <span class="icon-share-alt"></span>
-  # glyph(:lock, :white)
-  # # => <span class="icon-lock icon-white"></span>
-  # glyph(:thumbs_up, :pull_left)
-  # # => <i class="icon-thumbs-up pull-left"></i>
-  # glyph(:lock, {tag: :span})
-  # # => <span class="icon-lock"></span>
-  def glyph(*names)
-    names.map! { |name| name.to_s.gsub('_','-') }
-    names.map! do |name|
-      name =~ /pull-(?:left|right)/ ? name : "fa fa-#{name}"
-    end
-    # <i class="fa fa-camera-retro"></i> 
-    content_tag :i, nil, :class => names
+  def glyph(name)
+    name = name.to_s.gsub('_','-')
+    # <i class="fa fa-camera-retro" aria-hidden="true"></i>
+    content_tag :i, nil, class: "fa fa-#{name}", 'aria-hidden' => true
   end
 end
