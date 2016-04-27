3cf2a7d259905cd36586cfe26603cd711558cc9c
bamohan <binsunmaya@gmail.com>
2016-04-26 21:36:23 -0700

    [#27] Add seed data for recipes and associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,22 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+  user = User.find_by_email('seed_user@recipe.com') || User.create(email: 'seed_user@recipe.com',
+                                                                  password: 'testing123',
+                                                                  name: 'Seeded User')
+
+  recipe = Recipe.find_by_name("seeded recipe") || Recipe.create(name: "seeded recipe",
+                                                                 user: user,
+                                                                 picture: "some_picture.jpg")
+
+
+  Ingredient.find_by_value("Seeded Ingredient") || Ingredient.create(recipe: recipe,
+                                                                     value: "Seeded Ingredient")
+
+  Step.find_by_description("Seeded Step") || Step.create(recipe: recipe,
+                                                          description: "Seeded Step",
+                                                          idx: 1,
+                                                          picture: "some_step.jpg")
+  Reference.find_by_location("www.seed_reference.com") || Reference.create( recipe: recipe,
+                                                                            location: "www.seed_reference.com")
