66d742ee756d8d3efefcb3c73bcd9e809300cbbf
Katharina Peter <gaz@gmx.net>
2016-11-05 19:46:57 -0700

    css for print added

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -14,11 +14,11 @@ module RecipesHelper
   def edit_link(recipe)
     if allow_edit?(recipe)
       tooltip = t('.edit', default: t("helpers.links.edit"))
-      link_to edit_recipe_path(recipe), title: tooltip, data: { toggle: 'tooltip' } do
+      link_to edit_recipe_path(recipe), title: tooltip, class: 'hidden-print', data: { toggle: 'tooltip' } do
         edit_icon
       end
     else
-      link_to '#', disabled: true, class: 'disabled' do
+      link_to '#', disabled: true, class: 'disabled hidden-print' do
         edit_icon
       end
     end
@@ -37,11 +37,11 @@ module RecipesHelper
         toggle: 'tooltip'
       }
       tooltip = t('.destroy', default: t("helpers.links.destroy"))
-      link_to recipe_path(recipe), method: 'delete', data: data, id: 'delete_link', title: tooltip do
+      link_to recipe_path(recipe), method: 'delete', class: 'hidden-print',data: data, id: 'delete_link', title: tooltip do
         destroy_icon
       end
     else
-      link_to '#', data: { toggle: 'tooltip' }, title: 'Nope...', disabled: true, id: 'delete_link', class: 'disabled' do
+      link_to '#', data: { toggle: 'tooltip' }, title: 'Nope...', disabled: true, id: 'delete_link', class: 'disabled hidden-print' do
         destroy_icon
       end
     end
@@ -54,7 +54,7 @@ module RecipesHelper
   end
 
   def new_link
-    css_class = 'btn btn-success-outline btn-sm'
+    css_class = 'btn btn-success-outline btn-sm hidden-print'
     css_class << ' disabled' unless user_signed_in?
     link_to new_recipe_path do
       attr = { type: 'button', class: css_class }
