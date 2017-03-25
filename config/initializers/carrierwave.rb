d4d15e3956557a942699ee6616acceb212a579a9
Katharina Peter <gaz@gmx.net>
2017-03-25 13:53:00 -0700

    moved carrierwave config to initializers directory

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,12 +6,10 @@ CarrierWave.configure do |config|
     aws_secret_access_key: ENV['S3_SECRET'],
     region: ENV['S3_REGION']
   }
-
+  
   # For testing, upload files to local `tmp` folder.
-  if Rails.env.test? || Rails.env.cucumber?
+  if Rails.env.test? || Rails.env.development?
     config.storage = :file
-    config.enable_processing = false
-    config.root = "#{Rails.root}/tmp"
   else
     config.storage = :fog
   end
